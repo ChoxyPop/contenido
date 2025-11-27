4675126ba5f99f75d74f6ec9bbe6d774c5cd2842
Choxy <choxypop@gmail.com>
2025-11-27 17:32:03 -0300

    Update redirects.js

--- redirects.js
+++ redirects.js
@@ -1,18 +1,14 @@
 // Variable existente con algunos enlaces
 var redirects = {
-  "FB": "https://www.facebook.com/chochoxapp",
-  "fb": "https://www.facebook.com/chochoxapp",
-  "Noctrow": "https://tiktok.com/@noctrow" ,
-  "c080s9": "https://blog.chochoxapp.com/2024/12/actualizacion.html",
+  "fb": "https://www.facebook.com/choxypop.oficial",
+  "instagram": "https://instagram.com/choxypop",
   "wa": "https://whatsapp.com/channel/0029VaFZXi4FHWq38h6MIO0H",
-  "WA": "https://whatsapp.com/channel/0029VaFZXi4FHWq38h6MIO0H",
   "wagroup": "https://chat.whatsapp.com/J4gHptKxOZaGD81IcGzxDW", 
   "ia": "https://crushon.ai/es/character/714ca47f-39c0-45d7-8fbb-f4c3567ca7a6/details ",
-  "IA": "https://crushon.ai/es/character/714ca47f-39c0-45d7-8fbb-f4c3567ca7a6/details", 
   "stickers": "https://sticker.ly/s/Z61AWD", 
-  "Stickers": "https://sticker.ly/s/Z61AWD",
-  "deepcomics": "https://plus.chochoxapp.com/terminos-y-condiciones#deepcomics",
   "roblox": "https://www.roblox.com/es/users/432698417/profile",
-  "Roblox": "https://www.roblox.com/es/users/432698417/profile",
-  "comic": "https://plus.chochoxapp.com/2025/05/el-secreto-de-chochox-murasaky.html"
+  "minicomics": "https://pinterest.com/choxypop/minicomics",
+  "memes": "https://pinterest.com/choxypop/memes",
+  "fanarts": "https://pinterest.com/choxypop/fanarts",
+  "dibujos": "https://pinterest.com/choxypop/dibujos"
 };
