caa73930e9afd890a919faa8c690d26c5322b7bf
Choxy <choxypop@gmail.com>
2026-02-20 15:07:55 -0300

    Add new redirect links to redirects.js

--- redirects.js
+++ redirects.js
@@ -1,11 +1,13 @@
 // Variable existente con algunos enlaces
 var redirects = {
   "quieromichoxyluche": "https://www.roblox.com/es/catalog/138730439145028/Choxy-de-Peluche",
+  "wallpaper": "https://drive.google.com/drive/folders/1wgV6JUtpBvqiQ-5nHB2q4Uegk0GaNFKd",
+  "wallpapers": "https://drive.google.com/drive/folders/1wgV6JUtpBvqiQ-5nHB2q4Uegk0GaNFKd",
+  "facebook": "https://www.facebook.com/choxypop.oficial",
   "fb": "https://www.facebook.com/choxypop.oficial",
   "instagram": "https://instagram.com/choxypop",
   "wa": "https://whatsapp.com/channel/0029VaFZXi4FHWq38h6MIO0H",
   "wagroup": "https://chat.whatsapp.com/J4gHptKxOZaGD81IcGzxDW", 
-  "ia": "https://crushon.ai/es/character/714ca47f-39c0-45d7-8fbb-f4c3567ca7a6/details ",
   "stickers": "https://sticker.ly/s/Z61AWD", 
   "roblox": "https://www.roblox.com/es/users/432698417/profile",
   "minicomics": "https://pinterest.com/choxypop/minicomics",
